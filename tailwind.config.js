ef78d2003fc63bb2f4e03c04c3fdb31f084624f2
yasin taghavi <mrtqvi@yahoo.com>
2023-11-03 12:00:34 +0330

    add news and article section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '0.5rem',
+        xs: '1rem',
+        md: '0',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      }
+    },
+    fontFamily : {
+      'vazir' :  ["vazirmatn"],
+    },
+    extend: {
+      screens: {
+        'xs': '440px',
+      },
+    },
   },
   plugins: [],
 }
